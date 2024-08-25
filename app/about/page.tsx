b8faca84e1b228aeb55788297d6757969398bdf6
Mohit <thisisblinz@gmail.com>
2024-08-25 17:46:46 +0530

    code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,8 +9,8 @@ const About = () => {
             <h1 className="text-white font-bold text-2xl">About</h1>
             <p className="my-2">This is a meme generator made using Typescript, Next.js 14, NextUI, TailwindCSS and an open source mene generator api.</p>
             <div className="flex gap-2">
-                <Link href="https://github.com/iammohit4/meme-generator"><Button>Source Code</Button></Link>
-                <Link href="https://linkedin.com/in/mohit-sundrani-11470a309"><Button color="primary"><LuLinkedin /></Button></Link>
+                <Link href="https://github.com/iammohit4/meme-generator" target="_blank"><Button>Source Code</Button></Link>
+                <Link href="https://linkedin.com/in/mohit-sundrani-11470a309" target="_blank"><Button color="primary"><LuLinkedin /></Button></Link>
             </div>
         </>
     )
